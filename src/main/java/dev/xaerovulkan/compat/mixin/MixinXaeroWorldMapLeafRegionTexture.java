219a361708aa58a84b55c4b2d4d6f55901ea9a02
SourceNa₂CO₃Zn <125875649+safesaffawa@users.noreply.github.com>
2026-06-06 13:28:15 +0800

    Modify uploadNonCache method to use CallbackInfoReturnable

--- src/main/java/dev/xaerovulkan/compat/mixin/MixinXaeroWorldMapLeafRegionTexture.java
+++ src/main/java/dev/xaerovulkan/compat/mixin/MixinXaeroWorldMapLeafRegionTexture.java
@@ -4,15 +4,15 @@ import dev.xaerovulkan.compat.XaeroVulkanCompat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "xaero.map.region.texture.LeafRegionTexture", remap = false)
 public class MixinXaeroWorldMapLeafRegionTexture {
 
     @Inject(method = "uploadNonCache", at = @At("HEAD"), cancellable = true, remap = false)
-    private void xvcompat$onUploadNonCache(CallbackInfo ci) {
+    private void xvcompat$onUploadNonCache(CallbackInfoReturnable<Boolean> cir) {
         if (XaeroVulkanCompat.VULKAN_ACTIVE) {
-            ci.cancel();
+            cir.setReturnValue(false);
         }
     }
 }
