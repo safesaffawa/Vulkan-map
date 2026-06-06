a0541008ed512c60ad94d94ec99d47378cf0f3c5
SourceNa₂CO₃Zn <125875649+safesaffawa@users.noreply.github.com>
2026-06-06 13:22:12 +0800

    Refactor Mixin for RegionTexture and add new injections
    Refactor mixin class and update method injections for Vulkan compatibility.

--- src/main/java/dev/xaerovulkan/compat/mixin/MixinXaeroWorldMapLeafRegionTexture.java
+++ src/main/java/dev/xaerovulkan/compat/mixin/MixinXaeroWorldMapLeafRegionTexture.java
@@ -5,14 +5,29 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(targets = "xaero.map.region.texture.LeafRegionTexture", remap = false)
-public class MixinXaeroWorldMapLeafRegionTexture {
+@Mixin(targets = "xaero.map.region.texture.RegionTexture", remap = false)
+public class MixinXaeroWorldMapRegionTexture {
 
-    @Inject(method = "uploadNonCache", at = @At("HEAD"), cancellable = true, remap = false)
-    private void xvcompat$onUploadNonCache(CallbackInfo ci) {
+    @Inject(method = "ensureUnpackPBO", at = @At("HEAD"), cancellable = true, remap = false)
+    private void xvcompat$onEnsureUnpackPBO(CallbackInfo ci) {
         if (XaeroVulkanCompat.VULKAN_ACTIVE) {
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "writeToUnpackPBO", at = @At("HEAD"), cancellable = true, remap = false)
+    private void xvcompat$onWriteToUnpackPBO(CallbackInfo ci) {
+        if (XaeroVulkanCompat.VULKAN_ACTIVE) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "uploadBuffer", at = @At("HEAD"), cancellable = true, remap = false)
+    private void xvcompat$onUploadBuffer(CallbackInfoReturnable<Boolean> cir) {
+        if (XaeroVulkanCompat.VULKAN_ACTIVE) {
+            cir.setReturnValue(false);
         }
     }
 }
